a2ca7ef0cf32fdebdf70f245e4bb91f5d5c33c46
Sue Lee <sue.lee0308@gmail.com>
2021-10-22 22:25:22 -0500

    schema and seeds made. initial commit

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+DROP TABLE IF EXISTS employee_role;
+CREATE TABLE employee_role (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES employee_role(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,35 @@
+INSERT INTO department (name)
+VALUES  ("Accounting"),
+        ("Sales"),
+        ("Customer Service"),
+        ("Quality Assurance"),
+        ("Warehouse"),
+        ("Management");
+    
+INSERT INTO employee_role (title, salary, department_id)
+VALUES  ("Regional Manager", 120000, 6),
+        ("Co-Manager", 1000000, 6),
+        ("Receptionist", 65000, 3),
+        ("Customer Service Agent", 70000, 3),
+        ("Accountant", 1000000, 1),
+        ("Salesman", 80000, 2),
+        ("Quality Control Manager", 72000, 4),
+        ("Head Foreman", 85000, 5),
+        ("Warehouse Coordinator", 55000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Michael", "Scott", 1, NULL),
+        ("Jim", "Halpert", 2, 1),
+        ("Pamela", "Beasley", 3, 1),
+        ("Erin", "Hannon", 3, 1),
+        ("Kellly", "Kapoor", 4, 1),
+        ("Oscar", "Martinez", 5, 1),
+        ("Kevin", "Malone", 5, 1),
+        ("Angela", "Martin", 5, 1),
+        ("Dwight", "Schrute", 6, 1),
+        ("Stanley", "Hudson", 6, 1),
+        ("Phillis", "Vance", 6, 1),
+        ("Andy", "Bernard", 6, 1),
+        ("Creed", "Bratton", 7, 1),
+        ("Darryl", "Philban", 8, 1),
+        ("Roy", "Anderson", 9, 1);
